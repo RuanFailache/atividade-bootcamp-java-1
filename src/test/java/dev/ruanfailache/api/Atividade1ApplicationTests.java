dba6da975c55b1e6eb58db89aab986bee58560bc
Ruan Failache <ruanfailache@gmail.com>
2024-01-21 18:38:47 -0300

    init project

---
+++ src/test/java/dev/ruanfailache/api/Atividade1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruanfailache.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
